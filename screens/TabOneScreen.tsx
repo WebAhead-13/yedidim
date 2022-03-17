2184a24139c5d65c30b8e7229b96a9c599952bc4
“SalehK98” <salehkaloti@gmail.com>
2022-03-17 12:33:33 +0200

    intial cimmit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,81 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { List } from "react-native-paper";
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+const data = [
+  {
+    eventType: "פנצ׳ר",
+    carType: "סיטוראן ברלינגו",
+    isPrivateCar: true,
+    address: "דרך כרמל 63",
+    city: "ירושלים",
+    lat: "",
+    lng: "",
+    time: "14:54",
+  },
+  {
+    eventType: "שמן-מימ-דלק",
+    carType: "טויוטה קורולה",
+    isPrivateCar: true,
+    address: "םילשורי ,7 ןיול והירמש - מ״ק 1.5",
+    city: "ירושלים",
+    lat: "",
+    lng: "",
+    time: "14:00",
+  },
+  {
+    eventType: "חטש ץוליח",
+    carType: "סוקופ דרופ",
+    isPrivateCar: true,
+    address: "םילשורי ,29 םיכורב לאומש םכח - מ״ק 1.7",
+    city: "ירושלים",
+    lat: "",
+    lng: "",
+    time: "14:50",
+  },
+  {
+    eventType: "םילבכ/הענה",
+    carType: "סוקופ דרופ",
+    isPrivateCar: true,
+    address: "םילשורי ,1 הדוהי יבצ ברה - מ״ק 1.9",
+    city: "ירושלים",
+    lat: "",
+    lng: "",
+    time: "14:04",
+  },
+];
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    <FlatList
+      keyExtractor={(event) => event.time}
+      data={data}
+      renderItem={({ item }) => {
+        return (
+          <List.Item
+            title={item.eventType}
+            titleStyle={styles.title}
+            description={item.carType}
+            // style={styles.item}
+          />
+        );
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  item: {
+    backgroundColor: "white",
   },
   title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    backgroundColor: "#213C4F",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
